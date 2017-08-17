ffbdddeebf38bf0994a8498ac6840b3229f7273f
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 17:48:22 +0300

    Create index.blade.php

---
+++ app/views/index/index.blade.php
@@ -0,0 +1,8 @@
+<html>
+  <body>
+    <form action="{{URL::to('/make-url')}}}" method="POST">
+      <input type="text" name="url" />
+      <button type="submit">Make URL</button>
+ </form>
+  </body>
+</html>
