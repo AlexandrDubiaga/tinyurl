ebe387c048db17318f7c582119131758ff5db7b9
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 18:26:35 +0300

    Update IndexController.php

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,6 +8,11 @@ class IndexController extends BaseController
  public function posturl()
   {
     $url = Input::get('url');
+    $link = new Link();
+    $link->url = $url
+    $link->save();
+    return  $link->id;
+   
   }
 }
 ?>
