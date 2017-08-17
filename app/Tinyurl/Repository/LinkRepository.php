2f3618681cc65d860e50bf3f4c779b82956acd56
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 19:41:50 +0300

    Update LinkRepository.php

--- app/Tinyurl/Repository/LinkRepository.php
+++ app/Tinyurl/Repository/LinkRepository.php
@@ -1,5 +1,18 @@
 <?php
-
+namespace Tityurl\Repository;
+class LinkRepository
+{
+    public function create($url)
+    {
+        $link = new Link();
+        $link->url = $url;
+        $link->save(); 
+    }
+    public function find($id)
+    {
+      
+    }
+}
 
 
 ?>
