4ff4ac42d6dd770c0a73d16ca349b149bdc88ce1
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 19:44:23 +0300

    Update LinkRepository.php

--- app/Tinyurl/Repository/LinkRepository.php
+++ app/Tinyurl/Repository/LinkRepository.php
@@ -4,13 +4,19 @@ class LinkRepository
 {
     public function create($url)
     {
-        $link = new Link();
+        $link = new \Link();
         $link->url = $url;
         $link->save(); 
+        return $link->id;
     }
     public function find($id)
     {
-      
+      $link = \Link::find($id);
+        if(!$link)
+        {
+            return null;
+        }
+        return $link->url;
     }
 }
 
