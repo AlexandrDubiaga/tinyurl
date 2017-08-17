25fcfc00e28729b4af260d6cf63a7f2650d80bf7
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-17 19:22:04 +0300

    Update index.blade.php

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -3,6 +3,9 @@
     <form action="{{URL::to('/make-url')}}" method="POST">
       <input type="text" name="url" />
       <button type="submit">Make URL</button>
+      @if($errors->first('url'))
+      <p>{{{$errors->first('url')}}}</p>
+      @endif
  </form>
   </body>
 </html>
