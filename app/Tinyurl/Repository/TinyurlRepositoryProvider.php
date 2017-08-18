46fa3ea0ca4ddf7a827b6b6bf2121d69209df271
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-18 17:52:53 +0300

    Update TinyurlRepositoryProvider.php

--- app/Tinyurl/Repository/TinyurlRepositoryProvider.php
+++ app/Tinyurl/Repository/TinyurlRepositoryProvider.php
@@ -5,7 +5,7 @@ class TinyurlRepositoryProvider extends ServiceProvider
 {
   public function register()
   {
-    $this->app->bind('Tinyurl\Repository\Link\LinkRepositoryInterface', function(){ return new Tinyurl\Repository\Link\DbLinkRepository(new\Link);    });
+    $this->app->bind('Tinyurl\Repository\Link\LinkRepositoryInterface', function(){ return new \Tinyurl\Repository\Link\DbLinkRepository(new\Link);    });
   }
 }
 
