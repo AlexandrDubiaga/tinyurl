ea7b0f1ca2ecfb0fa3bd72eab0ea645c6adc071a
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-18 17:30:44 +0300

    Update app.php

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		
+		'Tinyurl\Repository\TinyurlRepositoryProvider',
 
 	),
 
