69f809c5240be768ef82d51fff6de6a60247a010
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-18 17:20:21 +0300

    Update DbLinkRepository.php

--- app/Tinyurl/Repository/Link/DbLinkRepository.php
+++ app/Tinyurl/Repository/Link/DbLinkRepository.php
@@ -1,4 +1,24 @@
 <?php
+namespace Tinyurl\Repository;
+class LinkRepository
+{
+    public function create($url)
+    {
+        $link = new \Link();
+        $link->url = $url;
+        $link->save(); 
+        return $link->id;
+    }
+    public function find($id)
+    {
+      $link = \Link::find($id);
+        if(!$link)
+        {
+            return null;
+        }
+        return $link->url;
+    }
+}
 
 
 
