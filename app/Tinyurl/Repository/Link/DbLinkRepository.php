b28e2203e5c6097469fd7b2695c0f75e02401b8f
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-18 17:48:32 +0300

    Update DbLinkRepository.php

--- app/Tinyurl/Repository/Link/DbLinkRepository.php
+++ app/Tinyurl/Repository/Link/DbLinkRepository.php
@@ -2,16 +2,20 @@
 namespace Tinyurl\Repository\Link;
 class DbLinkRepository implements LinkRepositoryInterface
 {
+    public function __construct($model)
+    {
+        $this->_model = $model;
+    }
     public function create($url)
     {
-        $link = new \Link();
+        $link =   $this->_model;
         $link->url = $url;
         $link->save(); 
         return $link->id;
     }
     public function find($id)
     {
-      $link = \Link::find($id);
+      $link =  $this->_model->find($id);
         if(!$link)
         {
             return null;
